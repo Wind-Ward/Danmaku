6f8c367a44bc0130d2f41f5ce02ea9993f250e2d
Wind-Ward <xuan223344@126.com>
2017-08-30 18:08:14 +0800

    first commit

---
+++ DataPreProcessing.py
@@ -0,0 +1,63 @@
+
+import numpy as np
+import copy
+from ReadBulletScreen import BulletScreen
+from collections import OrderedDict
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+
+class DataPreProcessing(object):
+    def __init__(self):
+        self.docSet=[]
+        self.docSize=[]
+
+
+
+    def _proxy_(self, file_name, POS_tag):
+        self.lines, vocabulary = BulletScreen().run(file_name, POS_tag)
+        vocabulary_list=[]
+        for line in self.lines:
+            _vocabulary = copy.copy(vocabulary)
+            for item in line["text"]:
+                if item in _vocabulary:
+                    _vocabulary[item]+=1
+
+            vocabulary_list.append(list(_vocabulary.values()))
+        self.print_raw_comment(self.lines)
+
+        # print(len(self.lines))
+        # print(len(vocabulary))
+
+        return self.lines,np.array(vocabulary_list),len(vocabulary),vocabulary
+
+
+
+    def print_raw_comment(self,lines):
+        with open("data/raw.txt","w") as f:
+            for line in lines:
+                f.write(" ".join(line["text"])+"\n")
+
+
+
+# def store_docset(docSet):
+#         fw = open("data/cache/docSet", "wb")
+#         pickle.dump(docSet, fw)
+#         fw.close()
+
+
+#file_name = "data/danmu/1.xml"
+if __name__=="__main__":
+
+    #file_name = "data/danmu/2.xml"
+    #file_name = "data/1_test.xml"
+    POS_tag = ["m", "w", "g", "c", "o", "p", "z", "q", "un", "e", "r", "x", "d", "t", "h", "k", "y", "u", "s", "uj",
+               "ul","r", "eng"]
+    lines=DataPreProcessing()._proxy_(file_name,POS_tag)
+
+    # lines
+    # [{'lineno': 2041, 'time': 0, 'text': ['小伙伴', '你们好']}, {'lineno': 2729, 'time': 0, 'text': ['伪装', '着看', '完']}, {'lineno': 4227, 'time': 0, 'text': ['僵尸', '极品']},
+
+
---
+++ ReadBulletScreen.py
@@ -0,0 +1,77 @@
+
+
+import re
+import jieba
+import jieba.posseg as pseg
+import copy
+
+from collections import OrderedDict
+
+class BulletScreen(object):
+    def __init__(self):
+        self.stop_words= set([
+                    " ","the","of","is","and","to","in","that","we","for",\
+                    "an","are","by","be","as","on","with","can","if","from","which","you",
+                    "it","this","then","at","have","all","not","one","has","or","that","什么","一个"
+                    ])
+
+
+    def load_stop_words(self,file="data/metadata/stopWords.txt"):
+        f = open(file)
+        content = f.read()
+        words = content.split('\n')
+        for w in words:
+            self.stop_words.add(w.strip())
+
+
+
+
+    def read(self,file_name,POS_tag):
+        f = open(file_name, "r")
+        tempLine=[]
+        vocabulary = OrderedDict()
+        jieba.load_userdict("data/metadata/user_dict.txt")
+        for lineNo,line in enumerate(f.readlines()):
+            pattern=re.compile("^<d p=\"(.+)\">(.+)</d>")
+            m=pattern.match(line)
+            if m:
+                temp={"time":int(float(m.group(1).split(',')[0])), \
+                                   "text":[word  for word,flag in pseg.cut(m.group(2))  \
+                                           if word not in self.stop_words and flag not in \
+                                           POS_tag ],
+                                   "lineno":lineNo+1}
+
+
+                #提取有效弹幕 有效弹幕为长度>3的弹幕
+                # if len(temp["text"])>=3:
+                #     print(temp["text"])
+                #     tempLine.append(temp)
+                #     for item in temp["text"]:
+                #         if item not in vocabulary:
+                #             vocabulary[item]=0
+
+
+        lines=sorted(tempLine, key= lambda e:(e.__getitem__('time')))
+        # print(vocabulary)
+        return lines,vocabulary
+
+
+    def run(self,file_name,POS_tag):
+        self.load_stop_words()
+        return  self.read(file_name,POS_tag)
+
+
+
+if __name__=="__main__":
+    # 所要分析的弹幕文件
+    file_name = "data/danmu/1.xml"
+    # 采用词性过滤的方式来过滤对弹幕挖掘没有实际意义的词 具体可查 http://www.cnblogs.com/adienhsuan/p/5674033.html
+    POS_tag = ["m", "w", "g", "c", "o", "p", "z", "q", "un", "e", "r", "x", "d", "t", "h", "k", "y", "u", "s", "uj",
+               "ul",
+               "r", "eng"]
+    print(BulletScreen().run(file_name,POS_tag))
+    #lines
+    #[{'lineno': 2041, 'time': 0, 'text': ['小伙伴', '你们好']}, {'lineno': 2729, 'time': 0, 'text': ['伪装', '着看', '完']}, {'lineno': 4227, 'time': 0, 'text': ['僵尸', '极品']},
+
+
+
